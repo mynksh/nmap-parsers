c3397f44895e96066467716a725701476e983f44
Mayank <mayank@securityium.com>
2025-08-15 19:43:09 +0100

    Update and rename subdomaintakeover.sh to ns_takeover_check.sh

--- ns_takeover_check.sh
+++ ns_takeover_check.sh
@@ -1,5 +1,5 @@
 #use below to find who is pointed by the domain
-while read h; do host "$h" | awk '/has address/ {print $4}' | while read ip; do echo "$h - $ip - $(whois $ip | grep -i 'OrgName')"; done; done < hosts.txt
+#while read h; do host "$h" | awk '/has address/ {print $4}' | while read ip; do echo "$h - $ip - $(whois $ip | grep -i 'OrgName')"; done; done < hosts.txt
 
 
 #!/usr/bin/env bash
